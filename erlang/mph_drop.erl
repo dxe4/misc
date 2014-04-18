54d155db7ab6c67bd1e4f163f52b746dea15e99d
charalambosp <papaloizouc@gmail.com>
2014-04-18 11:47:21 +0100

    fault tollerance in process

--- erlang/mph_drop.erl
+++ erlang/mph_drop.erl
@@ -1,7 +1,9 @@
 -module(mph_drop).
 -export([mph_drop/0]).
+
 mph_drop() ->
-  Drop=spawn(drop2,drop,[]),
+  process_flag(trap_exit, true),
+  Drop=spawn_link(drop2,drop,[]),
   convert(Drop).
 
 convert(Drop) ->
@@ -10,9 +12,36 @@ convert(Drop) ->
       Drop ! {self(), Planemo, Distance},
       convert(Drop);
 
+    {'EXIT', _Pid, _Reason} ->
+      NewDrop=spawn_link(drop2,drop,[]),
+      convert(NewDrop);
+
     {Planemo, Distance, Velocity} ->
       MphVelocity= 2.23693629 * Velocity,
-      io:format("On ~p, a fall of ~p meters yields a velocity of ~p mph.~n",
-        [Planemo, Distance, MphVelocity]),
+      io:format("On ~p, a fall of ~p meters yields a velocity of ~p mph.~n",[Planemo, Distance, MphVelocity]),
       convert(Drop)
-  end.
+end.
+
+
+%% -module(mph_drop).
+%% -export([mph_drop/0]).
+%%
+%% mph_drop() ->
+%%   process_flag(trap_exit, true),
+%%   Drop=spawn_link(drop2,drop,[]),
+%%   convert(Drop).
+%%
+%% convert(Drop) ->
+%%   receive
+%%     {Planemo, Distance} ->
+%%       Drop ! {self(), Planemo, Distance},
+%%       convert(Drop);
+%%
+%%     {'EXIT', Pid, Reason} ->
+%%       io:format("FAILURE: ~p died because of ~p.~n",[Pid, Reason]);
+%%
+%%     {Planemo, Distance, Velocity} ->
+%%       MphVelocity= 2.23693629 * Velocity,
+%%       io:format("On ~p, a fall of ~p meters yields a velocity of ~p mph.~n",[Planemo, Distance, MphVelocity]),
+%%       convert(Drop)
+%% end.

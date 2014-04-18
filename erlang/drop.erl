f8012e26f5ec91ad33c89d3a05984c200a2b83f3
charalambosp <papaloizouc@gmail.com>
2014-04-18 05:41:47 +0100

    erlang

--- erlang/drop.erl
+++ erlang/drop.erl
@@ -5,9 +5,24 @@ fall_velocity(Planemo, Distance) ->
               earth when Distance >= 0 -> 9.8;
               moon when Distance >= 0 -> 1.6;
               mars when Distance >= 0 -> 3.71
-            end, % note comma - function isn't done yet
-  math:sqrt(2 * Gravity * Distance).
+  end,
 
+  Velocity = math:sqrt(2 * Gravity * Distance),
+
+  Description = if
+    Velocity== 0 -> 'stable';
+    Velocity< 5 -> 'slow';
+    Velocity>= 5, Velocity < 10 -> 'moving';
+    Velocity>= 10, Velocity < 20 -> 'fast';
+    Velocity>= 20 -> 'speedy'
+  end,
+
+  if
+    (Velocity > 40) -> io:format("Look out below!~n");
+    true -> true
+  end,
+
+  Description.
 
 
 

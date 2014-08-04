8c5684c39f272b02a43c41f17508d661dc17e6ec
charalampos papaloizou <papaloizouc@gmail.com>
2014-08-04 01:05:55 +0100

    allow multiple files to be created in one run for less memory usage + clean up

--- opencv/capture.py
+++ opencv/capture.py
@@ -1,32 +1,38 @@
 import cv2
-# find the webcam
-capture = cv2.VideoCapture(0)
+import time
 
 
-fourcc = cv2.cv.CV_FOURCC(*'XVID')
-video_writer = cv2.VideoWriter("output.avi", fourcc, 25, (600, 600))
+def capture_for(seconds):
+    started = time.time()
+    fname = str(started).split('.')[0]
 
+    capture = cv2.VideoCapture(0)
+    fourcc = cv2.cv.CV_FOURCC(*'XVID')
+    video_writer = cv2.VideoWriter("out_{}.avi".format(fname),
+                                   fourcc, 25, (600, 600))
 
-# video recorder
-# cv2.VideoWriter_fourcc() does not exist
-# fourcc = cv2.cv.CV_FOURCC(*'XVID')
-# video_writer = cv2.VideoWriter("output.avi", -1, 20, (680, 480))
-
-# record video
-while(capture.isOpened()):
-    ret, frame = capture.read()
-    if ret == True:
-        frame = cv2.flip(frame, 0)
-
-        # write the flipped frame
-        video_writer.write(frame)
-
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    # record video
+    while(capture.isOpened()):
+        if started < time.time() - seconds:
             break
-    else:
-        break
 
-capture.release()
-# video_writer.release()
-cv2.destroyAllWindows()
+        ret, frame = capture.read()
+
+        if ret is True:
+            frame = cv2.flip(frame, 0)
+            # write the flipped frame
+            video_writer.write(frame)
+
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    duration = 10
+    capture_for(duration)

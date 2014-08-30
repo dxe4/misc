96759b9fbfb2d0f4d5ddb3744c9418b7ae3c15c5
charalampos papaloizou <papaloizouc@gmail.com>
2014-08-30 20:08:23 +0100

    pointers

---
+++ c_the_hard_way/pointers.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    char *names[] = {
+        "Alan", "Frank",
+        "Mary", "John", "Lisa"
+    };
+    int i = 0;
+    //wtf is goin on here, need some beard to figure out
+    int count = sizeof(names) / sizeof(*names);
+    char **cur_name = names;
+
+    printf("%d\n", count);
+
+    for(i = 0; i < count; i++) {
+        printf("name: %s\n", *(cur_name+i));
+    }
+
+    return 0;
+}
+/**
+type *ptr -> "a pointer of type named ptr"
+*ptr -> "the value of whatever ptr is pointed at"
+*(ptr + i) -> "the value of (whatever ptr is pointed at plus i)"
+&thing -> "the address of thing"
+type *ptr = &thing -> "a pointer of type named ptr set to the address of thing"
+ptr++ -> "increment where ptr points"
+**/

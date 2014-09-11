2fcce725d4846eee268a9970eaa27e5b7e4c9204
charalampos papaloizou <papaloizouc@gmail.com>
2014-09-11 21:33:11 +0100

    compiler errors

--- cuda_c344/ex3.cu
+++ cuda_c344/ex3.cu
@@ -283,11 +283,11 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
   //TODO: Call your convolution kernel here 3 times, once for each color channel.
   // TODO not sure if the output should be d_* probably a bug
   gaussian_blur<<<gridSize,blockSize>>>(d_redBlurred, d_redBlurred, numRows,
-                                       numCols, d_filter, filterWidth)
+                                       numCols, d_filter, filterWidth);
   gaussian_blur<<<gridSize,blockSize>>>(d_greenBlurred, d_greenBlurred, numRows,
-                                       numCols, d_filter, filterWidth)
+                                       numCols, d_filter, filterWidth);
   gaussian_blur<<<gridSize,blockSize>>>(d_blueBlurred, d_blueBlurred, numRows,
-                                       numCols, d_filter, filterWidth)
+                                       numCols, d_filter, filterWidth);
   // Again, call cudaDeviceSynchronize(), then call checkCudaErrors() immediately after
   // launching your kernel to make sure that you didn't make any mistakes.
   cudaDeviceSynchronize(); 
@@ -308,6 +308,7 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
   checkCudaErrors(cudaGetLastError());
 
   cleanup();
+}
 
 
 //Free all the memory that we allocated

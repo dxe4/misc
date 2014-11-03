ee869846f1d735078cec4b64b3da46956aec8667
charalampos papaloizou <papaloizouc@gmail.com>
2014-11-03 00:52:48 +0000

    fixed shared memory

--- cuda_c344/min_max_reduce.cu
+++ cuda_c344/min_max_reduce.cu
@@ -23,80 +23,76 @@ __device__ float MAX(float &x, float &y)
 }
 
 template<reduce_cb cb>
-__global__ void reduce(float *input, float *output, int *n)
+__global__ void reduce(float *input, float *output, int *n, int *nRows, int *nCols, int *blocksY)
 {
     /**
-    TODO, do some magic indexing with blocks
     __shared__ temp has a max size of 49152 b
     so the blokcs are split accordingly
     So now we need x values writen in outpu where x=blockSize.x * blockSize.y
     then we need to launch another kernel with input arr[x]
+    Every block executes 1 blelloch
     **/
     extern __shared__ float temp[];// allocated on invocation
+    __shared__ float last_elm;
 
     const int2 thread_2D_pos = make_int2( blockIdx.x * blockDim.x + threadIdx.x,
                                           blockIdx.y * blockDim.y + threadIdx.y);
-    const int thid = thread_2D_pos.y * (*n / *n) + thread_2D_pos.x;
+    const int thid = thread_2D_pos.y * ( *nCols ) + thread_2D_pos.x;
     int offset = 1;
+    const int b_thid = threadIdx.x;
 
-    int idx_a = 2 * thid;
-    int idx_b = idx_a + 1;
-    int idx_c = idx_b + 1;
-
-    if (idx_b >= *n)
+    if (2 * thid + 1 >= *n)
     {
         return;
     }
-    temp[idx_a] = input[idx_a]; // load input into shared memory
-    temp[idx_b] = input[idx_b];
+    temp[b_thid] = input[2 * thid]; // load input into shared memory
+    temp[b_thid + 1] = input[2 * thid + 1];
 
     __syncthreads();
-
     // build sum in place up the tree
-    for (int d = *n >> 1; d > 0; d >>= 1)
+    for (int d = 1024 >> 1; d > 0; d >>= 1)
     {
         __syncthreads();
-        if (thid < d)
+        if (b_thid < d)
         {
-            int ai = offset * (idx_b) - 1;
-            int bi = offset * (idx_c) - 1;
+            const int ai = offset * (b_thid + 1) - 1;
+            const int bi = offset * (b_thid + 2) - 1;
             temp[bi] = cb(temp[ai], temp[bi]);
         }
         offset *= 2;
     }
     // clear the last element
-    if (thid == 0)
+    if (b_thid == 0)
     {
-        temp[*n - 1] = 0.f;
+        last_elm = temp[1023];
+        temp[1023] = 0.f;
     }
-    offset = *n;
+    //offset = *n;
     // traverse down tree & build scan
-
-    for (int d = 1; d < *n; d *= 2)
+    __syncthreads();
+    for (int d = 1; d < 1024; d *= 2)
     {
         offset >>= 1;
         __syncthreads();
-        if (thid < d)
+        if (b_thid < d)
         {
-            int ai = offset * (idx_b) - 1;
-            int bi = offset * (idx_c) - 1;
+            int ai = offset * (b_thid + 1) - 1;
+            int bi = offset * (b_thid + 2) - 1;
 
             if (ai >= 0 && bi >= 0)
             {
                 float t = temp[ai];
                 temp[ai] = temp[bi];
-                // temp[bi] += t;
                 temp[bi] = cb(t, temp[bi]);
             }
         }
     }
     __syncthreads();
     // write results to device memory
-    output[idx_a] = temp[2 * thid];
-    output[idx_b] = temp[idx_b];
-    if (thid == 0)
+    if (b_thid == 0)
     {
-        output[*n] = cb(temp[*n - 1], input[*n - 1]);
+        int index = (blockIdx.x * (*blocksY ) + blockIdx.y);
+        output[index] = cb(temp[1023], last_elm);
     }
 }
 
@@ -118,7 +114,7 @@ int shared_memory_per_block()
 
 int main(int argc, char **argv)
 {
-    int numCols = 2048;
+    int numCols = 1536;
     int numRows = 1024;
 
     int ARRAY_SIZE = numCols * numRows;
@@ -140,23 +136,29 @@ int main(int argc, char **argv)
     float *d_out;
     int _h_in = ARRAY_SIZE;
     int *h_n = &_h_in;
-    int *d_n;
-
-    checkCudaErrors(cudaMalloc((void **) &d_in, ARRAY_BYTES));
-    checkCudaErrors(cudaMalloc((void **) &d_out, ARRAY_BYTES + sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **) &d_n, sizeof(int)));
-
-    checkCudaErrors(cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_n, h_n, sizeof(int), cudaMemcpyHostToDevice));
-
+    int *d_n, *nRows, *nCols, *blocksY;
 
     // this is the amount of __shared__ we can use
     int sharedMemPerBlock = shared_memory_per_block();
     // Split kernels to match the cache size in a square
     int blocks = (int)floor(sqrt(ARRAY_BYTES / sharedMemPerBlock));
+
+    checkCudaErrors(cudaMalloc((void **) &d_in, ARRAY_BYTES));
+    checkCudaErrors(cudaMalloc((void **) &d_out, ARRAY_BYTES + sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **) &d_n, sizeof(int)));
+    checkCudaErrors(cudaMalloc((void **) &nCols, sizeof(int)));
+    checkCudaErrors(cudaMalloc((void **) &nRows, sizeof(int)));
+    checkCudaErrors(cudaMalloc((void **) &blocksY, sizeof(int)));
+
+    checkCudaErrors(cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_n, h_n, sizeof(int), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(nRows, &numRows, sizeof(int), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(nCols, &numCols, sizeof(int), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(blocksY, &blocks, sizeof(int), cudaMemcpyHostToDevice));
+
     dim3 blockSize(blocks, blocks, 1);
     // good luck here +++
-    reduce<MAX> <<< blockSize, 1024, sharedMemPerBlock>>>(d_in, d_out, d_n);
+    reduce<MAX> <<< blockSize, 1024, 1024 * sizeof(float)>>>(d_in, d_out, d_n, nRows, nCols, blocksY);
     cudaThreadSynchronize();
 
     // check for error
@@ -188,3 +190,4 @@ int main(int argc, char **argv)
 
     return 0;
 }
+//http://blog.codinghorror.com/content/images/uploads/2008/08/6a0120a85dcdae970b012877705d12970c-pi.jpg

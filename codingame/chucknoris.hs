8270006448b23c7536ae37f8a5450d77e2792c7b
Charalampos Papaloizou <papaloizouc@gmail.com>
2014-12-31 20:59:56 +0000

    fixed chuck norris

--- codingame/chucknoris.hs
+++ codingame/chucknoris.hs
@@ -24,6 +24,15 @@ cacl_out prev (n:ns)
     | null(ns) ==True = [(last_num prev n)]
     | n /= prev = (make_key n):(cacl_out n ns)
     | otherwise = "0":(cacl_out n ns)
+    
+process_input (c:cn) previous
+    |null(cn) == True = res2
+    | otherwise = res2 ++ (process_input cn (last res))
+    where
+        num = fromIntegral (ord c) :: Float
+        res = solve' num 64.0
+        res2 = cacl_out previous res
+ 
 
 main = do
     hSetBuffering stdout NoBuffering -- DO NOT REMOVE
@@ -34,13 +43,11 @@ main = do
     message <- getLine
     
     -- hPutStrLn stderr "Debug messages..."
-    
+    hPutStrLn stderr message
     let bytes = reverse [ 2**x | x <- [0..6]]
-    let char = head message
-    let num = fromIntegral (ord char) :: Float
+    --- let char = head message
+    ---let num = fromIntegral (ord char) :: Float
+    let result = process_input message 999
 
-    let res = solve' num 64.0
-    let res2 = cacl_out 999 res
-    let z = intercalate "" res2
-    -- Write answer to stdout
+    let z = intercalate "" result
     putStrLn (tail z)
